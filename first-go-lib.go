e667769f9380013d1a916594a251b5e1574131a2
Oleksandr Aloshchenko <oleksandr_aloshchenko@epam.com>
2022-09-11 19:25:54 +0300

    the first commit

---
+++ first-go-lib.go
@@ -0,0 +1,16 @@
+/**
+ * Author: Alex Aloshchenko
+ * File: first-go-lib.go
+ */
+
+package lib
+
+// Add Returns the sum of two numbers
+func Add(a int, b int) int {
+	return a + b
+}
+
+// Subtract Returns the difference between two numbers
+func Subtract(a int, b int) int {
+	return a - b
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OlexSP/first-go-lib
+
+go 1.19
