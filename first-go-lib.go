3265825e0b17b0b08ec7185b7073fdca58d8bd3c
Oleksandr Aloshchenko <oleksandr_aloshchenko@epam.com>
2022-09-12 19:08:07 +0300

    + overall package description + integer overflow check + Multiply() + Division()

--- first-go-lib.go
+++ first-go-lib.go
@@ -1,16 +1,61 @@
 /**
  * Author: Alex Aloshchenko
  * File: first-go-lib.go
+ * Package description: The package with simple addition, subtraction, division and multiplication functions.
+ *				If a math operation causes integer overflow, the function returns: 0, error.
+ *				The package is designed to learn the process of creating third-party Golang packages.
  */
 
 package lib
 
-// Add Returns the sum of two numbers
-func Add(a int, b int) int {
-	return a + b
+import (
+	"errors"
+	"math"
+)
+
+var ErrOverflow = errors.New("integer overflow")
+
+// Add Returns the sum of two integer numbers.
+// In case of integer overflow, the function returns: 0, error.
+func Add(a int, b int) (int, error) {
+	if b > 0 {
+		if a > math.MaxInt-b {
+			return 0, ErrOverflow
+		}
+	} else {
+		if a < math.MinInt-b {
+			return 0, ErrOverflow
+		}
+	}
+	return a + b, nil
 }
 
-// Subtract Returns the difference between two numbers
-func Subtract(a int, b int) int {
-	return a - b
+// Subtract Returns the difference between two integer numbers.
+// In case of integer overflow, the function returns: 0, error.
+func Subtract(a int, b int) (int, error) {
+	if b > 0 {
+		if a < math.MinInt+b {
+			return 0, ErrOverflow
+		}
+	} else {
+		if a > math.MaxInt+b {
+			return 0, ErrOverflow
+		}
+	}
+	return a - b, nil
+}
+
+// Multiply Returns the multiplication of two integer numbers.
+// In case of integer overflow, the function returns: 0, error.
+func Multiply(a int, b int) (int, error) {
+	if b > math.MaxInt/a || a > math.MaxInt/b {
+		return 0, ErrOverflow
+	}
+	return a * b, nil
+}
+
+// Division  Returns the division of two integer numbers.
+// Only quotient is returned, remainder is ignored.
+func Division(a int, b int) int {
+	return a / b
 }
