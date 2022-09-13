dcd28b26c8753a6933c9d4d42beb97c33c297894
Oleksandr Aloshchenko <oleksandr_aloshchenko@epam.com>
2022-09-13 16:29:57 +0300

    + Test_Multiply() + Test_Division()

--- first-go-lib_test.go
+++ first-go-lib_test.go
@@ -51,3 +51,34 @@ func Test_Subtract(t *testing.T) {
 		}
 	}
 }
+
+func Test_Multiply(t *testing.T) {
+	tests := map[string]struct {
+		a      int
+		b      int
+		exp    int
+		expErr error
+	}{
+		"success":      {a: 5, b: 3, exp: 15, expErr: nil},
+		"int overflow": {a: 2, b: math.MaxInt, exp: 0, expErr: ErrOverflow},
+	}
+	for name, data := range tests {
+		got, err := Multiply(data.a, data.b)
+		if err != nil && err != data.expErr {
+			t.Errorf("[%s] unexpected error. expected:\"%s\", got:\"%s\"", name, data.expErr, err)
+		}
+		if got != data.exp || err != data.expErr {
+			t.Errorf("[%s] expected: %v, %v; got %v, %v", name, data.exp, data.expErr, got, err)
+		}
+	}
+}
+
+func Test_Division(t *testing.T) {
+	a := 10
+	b := 2
+	expected := a / b
+
+	if got := Division(a, b); got != expected {
+		t.Errorf("Add(%d, %d) = %d, didn't return %d", a, b, got, expected)
+	}
+}
