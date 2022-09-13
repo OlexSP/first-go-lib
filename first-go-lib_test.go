72c27f7f5b804af5ff8cc2a450e65a77ed5248d7
Oleksandr Aloshchenko <oleksandr_aloshchenko@epam.com>
2022-09-13 15:59:55 +0300

    + Test_Add() + Test_Subtract()

--- first-go-lib_test.go
+++ first-go-lib_test.go
@@ -6,25 +6,48 @@
 package lib
 
 import (
+	"math"
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
-	a := 1
-	b := 2
-	expected := a + b
-
-	if got := Add(a, b); got != expected {
-		t.Errorf("Add(%d, %d) = %d, didn't return %d", a, b, got, expected)
+func Test_Add(t *testing.T) {
+	tests := map[string]struct {
+		a      int
+		b      int
+		exp    int
+		expErr error
+	}{
+		"success":      {a: 3, b: 5, exp: 8, expErr: nil},
+		"int overflow": {a: math.MaxInt, b: 5, exp: 0, expErr: ErrOverflow},
+	}
+	for name, data := range tests {
+		got, err := Add(data.a, data.b)
+		if err != nil && err != data.expErr {
+			t.Errorf("[%s] unexpected error. expected:\"%s\", got:\"%s\"", name, data.expErr, err)
+		}
+		if got != data.exp || err != data.expErr {
+			t.Errorf("[%s] expected: %v, %v; got %v, %v", name, data.exp, data.expErr, got, err)
+		}
 	}
 }
 
-func TestSubtract(t *testing.T) {
-	a := 1
-	b := 2
-	expected := a - b
-
-	if got := Subtract(a, b); got != expected {
-		t.Errorf("Subtract(%d, %d) = %d, didn't return %d", a, b, got, expected)
+func Test_Subtract(t *testing.T) {
+	tests := map[string]struct {
+		a      int
+		b      int
+		exp    int
+		expErr error
+	}{
+		"success":      {a: 5, b: 3, exp: 2, expErr: nil},
+		"int overflow": {a: -5, b: math.MaxInt, exp: 0, expErr: ErrOverflow},
+	}
+	for name, data := range tests {
+		got, err := Subtract(data.a, data.b)
+		if err != nil && err != data.expErr {
+			t.Errorf("[%s] unexpected error. expected:\"%s\", got:\"%s\"", name, data.expErr, err)
+		}
+		if got != data.exp || err != data.expErr {
+			t.Errorf("[%s] expected: %v, %v; got %v, %v", name, data.exp, data.expErr, got, err)
+		}
 	}
 }
